446a36b8cec106f32690d430140ba0a2b52c4ee4
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-07-26 22:23:14 +0300

    done test selsup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Param {
+	id: number;
+	name: string;
+	type: string;
+}
+interface ParamValue {
+	paramId: number,
+	value: string
+}
+
+interface Props {
+	params: Param[],
+	model: ParamValue[]
+	setModel: React.Dispatch<React.SetStateAction<ParamValue[]>>
+}
+
+
+
+const initStateParams = [
+	{
+		id: 1,
+		name: "Назначение",
+		type: 'string'
+	},
+	{
+		id: 2,
+		name: "Длина",
+		type: 'string'
+	}
+]
+
+const initStateModels = [
+	{
+		paramId: 1,
+		value: "повседневное"
+	},
+	{
+		paramId: 2,
+		value: "макси"
+	}
+]
+
+
+const ParamEditor: React.FC<Props> = ({params, model, setModel}) => {
+	const changeValueHandler = (e: React.ChangeEvent<HTMLInputElement>, id: number): void => {
+		const {value} = e.target
+		setModel(
+			model.map(item => 
+				item.paramId === id ? {...item, value} : item
+			)
+		)
+	}
+
+	const unit = (id: number): ParamValue[] => {
+		return model.filter(item => item.paramId === id)
+	}
+	return(
+		<div>
+			{
+				params.map(param => (
+					<label key={param.id} className='flex gap-2 justify-between w-80 mb-2'>
+						<h4>
+							{param.name}
+						</h4>
+						<input
+							onChange={(e) => changeValueHandler(e, param.id)}
+							className='border-[1px] border-black'
+							type={param.type} 
+							name={param.name}
+							value={unit(param.id)[0].value}
+						/>
+					</label>
+				))
+			}
+		</div>
+	)
+}
+
+const App: React.FC = () => {
+	const [params, setParams] = useState<Param[]>(initStateParams)
+	const [model, setModel] = useState<ParamValue[]>(initStateModels)
+
+	const getModel = () => {
+		const structure = {
+			params: {...params},
+			model: {...model}
+		}
+		console.log(structure)
+	}
+
+
+	return (
+		<div className='flex items-center jusityf-center h-screen w-full '>
+			<div className='flex flex-col items-center justify-center w-full'>
+				<div>
+					<ParamEditor params={params} model={model} setModel={setModel}/>
+					<button 
+						onClick={getModel}
+						className='border-[1px] border-black rounded-full px-4 py-1 hover:bg-black hover:text-white transition'
+					>
+						Получить Структуру в консоль
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App;
